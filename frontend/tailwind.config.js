4d5b0c1ce0b1547d311d52c87f999d40ff634a20
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2025-01-31 17:57:19 +0300

    project updated

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // Tailwind'in React bileşenlerini tarayacağı yerler
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
